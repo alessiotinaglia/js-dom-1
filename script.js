c98e7c5ecabd98e3028d139d2357d901b4438430
alessiotinaglia <alessio.tina1998@gmail.com>
2024-11-07 15:49:52 +0100

    fine es con i bonus

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 "use strict"
 console.clear();
 
+const buttomImg = document.getElementById("cambiaImg");
+console.log(buttomImg);
+
+const firstImg = document.querySelector("img");
+console.log(firstImg);
+
+buttomImg.addEventListener('click', accendiSpegni);
+
+function accendiSpegni () {
+    if (buttomImg.textContent.includes("Accendi")) {
+        firstImg.src = "img/yellow_lamp.png";  
+        buttomImg.textContent = "Spegni";  
+        console.log("Spegni");  
+    } else {
+        firstImg.src = "img/white_lamp.png"; 
+        buttomImg.textContent = "Accendi"; 
+        console.log("Accendi");
+    }
+}
+
+
+
+
+
+
+
 
